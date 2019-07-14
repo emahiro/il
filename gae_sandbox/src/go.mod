f76c773a67b818686f16f1306d4b196ba2f77ad1
emahiro <hiromichi.a.ema@dena.com>
2019-07-14 15:51:19 +0900

    add server

---
+++ gae_sandbox/src/go.mod
@@ -0,0 +1,6 @@
+module emahiro/il/gae_sandbox
+
+require (
+	github.com/fharding1/gemux v0.2.2 // indirect
+	github.com/labstack/gommon v0.2.9 // indirect
+)
---
+++ gae_sandbox/src/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/fharding1/gemux"
+)
+
+var addr = 8080
+
+func main() {
+	mux := &gemux.ServeMux{}
+	mux.Handle("/hello", http.MethodGet, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hello"))
+	}))
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", addr),
+		Handler: mux,
+	}
+
+	log.Printf("server start port: %d ...", addr)
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("server closed with error: %v", err)
+		}
+	}()
+
+	// graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, os.Interrupt)
+	log.Printf("SIGNAL %d received, server shutdown ...\n", <-quit)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("failed to gracefully shutdown. err: %v", err)
+		os.Exit(1)
+	}
+	log.Printf("server shutdown....")
+}
