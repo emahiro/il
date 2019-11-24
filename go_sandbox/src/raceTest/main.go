ce9ae37e57a3a2c2494888436f077aa0d4650a19
emahiro <hiromichi.a.ema@dena.com>
2019-11-25 02:09:56 +0900

    add lock

--- go_sandbox/src/raceTest/main.go
+++ go_sandbox/src/raceTest/main.go
@@ -1,14 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-var m = make(map[string]int64, 0)
+var (
+	m  = make(map[string]int64, 0)
+	mu = sync.Mutex{}
+)
 
 func main() {
 	fmt.Printf("test")
 }
 
 func race(key string, val int64) {
+	mu.Lock()
+	defer mu.Unlock()
 	fmt.Printf("key = %v, val = %v\n", key, val)
 	m[key] = val
 	fmt.Printf("m = %v\n", m)
