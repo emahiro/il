6a6a3d9a1cc87d847248141f5c0b7ed90304072a
emahiro <hiromichi.a.ema@dena.com>
2020-01-08 03:07:15 +0900

    add getAndSet method

--- go_sandbox/cbFuncExample/main.go
+++ go_sandbox/cbFuncExample/main.go
@@ -5,11 +5,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"strings"
+	"sync"
 	"syscall"
 	"time"
 )
@@ -20,6 +22,14 @@ type user struct {
 	ID   int64  `json:"id"`
 	Name string `json:"name"`
 }
+
+type cache struct {
+	data map[string][]byte
+	exp  int64
+	mu   sync.Mutex
+}
+
+var c = &cache{data: make(map[string][]byte)}
 
 func main() {
 	mux := http.NewServeMux()
@@ -41,6 +51,19 @@ func main() {
 		}
 		w.WriteHeader(http.StatusOK)
 		w.Write(buf.Bytes())
+	})
+	mux.HandleFunc("/cache", func(w http.ResponseWriter, r *http.Request) {
+		f := func() ([]byte, error) {
+			return getUser()
+		}
+		b, err := c.getAndSetAfterFuncCall("test", f)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("error: %v", err)))
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(b)
 	})
 
 	server := &http.Server{
@@ -68,4 +91,57 @@ func main() {
 
 	log.Println("success to shutdown server")
 	os.Exit(0)
+}
+
+func getUser() ([]byte, error) {
+	cli := http.DefaultClient
+	resp, err := cli.Get("http://localhost:8080/user")
+	defer resp.Body.Close()
+
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("reqest failed. code: %d", resp.StatusCode)
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func (c *cache) getAndSetAfterFuncCall(key string, f func() ([]byte, error)) ([]byte, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c == nil {
+		return nil, fmt.Errorf("error: attempting to access nil cache object")
+	}
+
+	if len(key) == 0 {
+		return nil, fmt.Errorf("error: key is empty")
+	}
+
+	if c.data == nil {
+		return nil, fmt.Errorf("error: attempting to access nil map")
+	}
+
+	if time.Now().Unix() < c.exp {
+		if data, ok := c.data[key]; ok {
+			log.Println("use local cache")
+			return data, nil
+		}
+	}
+
+	b, err := f()
+	if err != nil {
+		return nil, err
+	}
+
+	c.data[key] = b
+	c.exp = time.Now().Add(10 * time.Second).Unix()
+
+	return b, nil
 }
