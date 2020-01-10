6029c59ac7baa107423aa552cea1afeaa65912c7
emahiro <hiromichi.a.ema@dena.com>
2020-01-11 03:26:52 +0900

    add github.com/pkg/profile

--- go_sandbox/groutineprofilesample/go.mod
+++ go_sandbox/groutineprofilesample/go.mod
@@ -1,3 +1,5 @@
 module github.com/emahiro/groutineprofilesample
 
 go 1.13
+
+require github.com/pkg/profile v1.4.0
--- go_sandbox/groutineprofilesample/main.go
+++ go_sandbox/groutineprofilesample/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/pkg/profile"
+)
 
 func main() {
+	defer profile.Start(profile.GoroutineProfile, profile.ProfilePath(".")).Stop()
 	fmt.Println("hello")
 }
