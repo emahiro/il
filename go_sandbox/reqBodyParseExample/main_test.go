35ef865a167539e9ce931772e6c5b9be2eaabcd5
emahiro <ema.hiro@gmail.com>
2020-03-25 00:40:45 +0900

    main: add failed test by escaped json string

--- go_sandbox/reqBodyParseExample/main_test.go
+++ go_sandbox/reqBodyParseExample/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -19,6 +20,15 @@ func TestHandler(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		d, _ := httputil.DumpRequest(r, true)
 		t.Log(string(d))
+
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		var req req
+		if err := json.Unmarshal(b, &req); err != nil {
+			t.Fatal(err)
+		}
 	}))
 	defer ts.Close()
 
