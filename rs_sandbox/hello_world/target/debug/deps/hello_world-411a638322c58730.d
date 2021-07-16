3c5f28c0dcbbaaa3b496548e2e4bf77ab7c429cf
emahiro <ema.hiro@gmail.com>
2021-07-16 17:35:25 +0900

    add: rs_sandbox

---
+++ rs_sandbox/hello_world/target/debug/deps/hello_world-411a638322c58730.d
@@ -0,0 +1,5 @@
+/Users/hiromichi.ema/src/github.com/emahiro/il/rs_sandbox/hello_world/target/debug/deps/hello_world-411a638322c58730.rmeta: src/main.rs
+
+/Users/hiromichi.ema/src/github.com/emahiro/il/rs_sandbox/hello_world/target/debug/deps/hello_world-411a638322c58730.d: src/main.rs
+
+src/main.rs:
---
+++ rs_sandbox/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+/Users/hiromichi.ema/src/github.com/emahiro/il/rs_sandbox/hello_world/target/debug/deps/hello_world: src/main.rs
+
+/Users/hiromichi.ema/src/github.com/emahiro/il/rs_sandbox/hello_world/target/debug/deps/hello_world.d: src/main.rs
+
+src/main.rs:
