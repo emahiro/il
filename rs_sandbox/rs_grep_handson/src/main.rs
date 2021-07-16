9d7478a5d4ab733109c0476a7c4e329a71b0c53b
emahiro <ema.hiro@gmail.com>
2021-07-16 21:02:56 +0900

    use structopt & use 借用

--- rs_sandbox/rs_grep_handson/src/main.rs
+++ rs_sandbox/rs_grep_handson/src/main.rs
@@ -1,36 +1,42 @@
-use std::{env::args, fs::read_to_string};
+use std::fs::read_to_string;
+use structopt::StructOpt;
 
+#[derive(StructOpt)] // attribute cf. annotation
+#[structopt(name = "rsgrep")]
 struct GrepArgs {
+    #[structopt(name="PATTERN")]
     pattern: String,
+    #[structopt(name="NAME")]
     path: String,
 }
 
-impl GrepArgs {
-    fn new(pattern: String, path: String) -> Self {
-        GrepArgs { pattern, path }
-    }
-}
+// impl GrepArgs {
+//     fn new(pattern: String, path: String) -> Self {
+//         GrepArgs { pattern, path }
+//     }
+// }
 
-fn grep(content: String, pattern: String) {
+fn grep(content: String, state: &GrepArgs) {
     for line in content.lines() {
-        if line.contains(pattern.as_str()) {
+        if line.contains(state.pattern.as_str()) {
             println!("{}", line)
         }
     }
 }
 
 fn run(state: GrepArgs) {
-    match read_to_string(state.path) {
-        Ok(content) => grep(content, state.pattern),
+    match read_to_string(&state.path) {
+        Ok(content) => grep(content, &state),
         Err(err) => println!("{}", err),
     }
 }
 
 fn main() {
-    let pattern = args().nth(1);
-    let path = args().nth(2);
-    match (pattern, path) {
-        (Some(pattern), Some(path)) => run(GrepArgs::new(pattern, path)),
-        _ => println!("pattern or path is not specified"),
-    };
+    run(GrepArgs::from_args())
+    // let pattern = args().nth(1);
+    // let path = args().nth(2);
+    // match (pattern, path) {
+    //     (Some(pattern), Some(path)) => run(GrepArgs::new(pattern, path)),
+    //     _ => println!("pattern or path is not specified"),
+    // };
 }
