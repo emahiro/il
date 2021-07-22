11b320e71afda83184aeeb1ae4cb31a328ed8521
emahiro <ema.hiro@gmail.com>
2021-07-23 00:52:11 +0900

    fix: fizzbuzz

--- rs_sandbox/rust_handson_20210716/src/main.rs
+++ rs_sandbox/rust_handson_20210716/src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
-    let num = 10;
-    let fizzbuss = if num == 15 {
-        "FizzBuzz".to_string()
-    } else if num == 5 {
-        "Buzz".to_string()
-    } else{
-        "Fizz".to_string()
-    };
-    println!("{}", fizzbuss);
+    for num in 1..100 {
+        let fizzbuss = if num % 15 == 0 {
+            "FizzBuzz".to_string()
+        } else if num % 5 == 0 {
+            "Buzz".to_string()
+        } else {
+            "Fizz".to_string()
+        };
+        println!("{}", fizzbuss);
+    }
 }
