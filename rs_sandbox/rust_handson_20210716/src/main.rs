b2ad4a9daa16997ee0ab619d915abe4c564966e1
emahiro <ema.hiro@gmail.com>
2021-07-22 19:28:39 +0900

    handson やり直し

--- rs_sandbox/rust_handson_20210716/src/main.rs
+++ rs_sandbox/rust_handson_20210716/src/main.rs
@@ -1,19 +1,3 @@
-fn fizzbuzz(num:i32) -> String{
-    if num % 15 == 0 {
-        "FizzBuzz".to_string()
-    } else if num % 5 == 0 {
-        "Fizz".to_string()
-    } else if num % 3 == 0 {
-        "Buzz".to_string()
-    } else {
-        num.to_string()
-    }
-}
 fn main() {
-    let result = (0..100).map(fizzbuzz).fold(String::from(""),|acc, line|format!("{}\n{}",acc,line));
-    println!("{}", result)
-
-    // for num in 0..100 {
-    //     println!("{}", fizzbuzz(num));
-    // }
+    println!("test")
 }
