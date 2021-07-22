0a06fea028aaac18a6e3d7ce5f5c67504e971f2f
emahiro <ema.hiro@gmail.com>
2021-07-23 01:03:28 +0900

    add: input number is from command line args

--- rs_sandbox/rust_handson_20210716/src/main.rs
+++ rs_sandbox/rust_handson_20210716/src/main.rs
@@ -1,12 +1,15 @@
+use std::env::{self};
+
 fn main() {
-    for num in 1..100 {
-        let fizzbuss = if num % 15 == 0 {
-            "FizzBuzz".to_string()
-        } else if num % 5 == 0 {
-            "Buzz".to_string()
-        } else {
-            "Fizz".to_string()
-        };
-        println!("{}", fizzbuss);
-    }
+    let args: Vec<String> = env::args().collect();
+    let numstr = &args[1].to_string();
+    let num: i32 = numstr.parse().unwrap();
+    let fizzbuss = if num % 15 == 0 {
+        "FizzBuzz".to_string()
+    } else if num % 5 == 0 {
+        "Buzz".to_string()
+    } else {
+        "Fizz".to_string()
+    };
+    println!("{}", fizzbuss);
 }
