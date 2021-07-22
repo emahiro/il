3455639cd4b3daf373e3afb471bd573287113533
emahiro <ema.hiro@gmail.com>
2021-07-22 19:37:47 +0900

    add: fizzbuzz

--- rs_sandbox/rust_handson_20210716/src/main.rs
+++ rs_sandbox/rust_handson_20210716/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("test")
+    let num = 10;
+    let fizzbuss = if num == 15 {
+        "FizzBuzz"
+    } else if num == 5 {
+        "Buzz"
+    } else{
+        "Fizz"
+    };
+    println!("{}", fizzbuss);
 }
