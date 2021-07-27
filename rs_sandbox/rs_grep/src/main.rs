af435fb871656f2e222a232b4829317ac00bd6a6
emahiro <ema.hiro@gmail.com>
2021-07-28 01:25:30 +0900

    add: plain file searcher

--- rs_sandbox/rs_grep/src/main.rs
+++ rs_sandbox/rs_grep/src/main.rs
@@ -1,3 +1,26 @@
+use std::{env::args, fs::read_to_string};
+
+fn grep(content: String, pattern: String) {
+    for line in content.lines() {
+        if line.contains(pattern.as_str()) {
+            println!("{}\n", line);
+        }
+    }
+}
+
+fn run(filepath: String, pattern: String) {
+    match read_to_string(filepath) {
+        Ok(content) => grep(content, pattern),
+        Err(error) => println!("error: {}\n", error),
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pattern = args().nth(1);
+    let filepath = args().nth(2);
+
+    match (pattern, filepath) {
+        (Some(pattern), Some(filepath)) => run(filepath, pattern),
+        _ => println!("Pattern or path is not specified.\n"),
+    };
 }
