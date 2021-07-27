e13330f408851faaf71373fe88d9595a3048b4e0
emahiro <ema.hiro@gmail.com>
2021-07-27 21:00:58 +0900

    add: sub method

--- rs_sandbox/rs_cat_handson/src/main.rs
+++ rs_sandbox/rs_cat_handson/src/main.rs
@@ -1,9 +1,13 @@
 use std::fs::read_to_string;
 
-fn main() {
-    let path = "./src/main.rs".to_string();
+fn run_cat(path: String) {
     match read_to_string(path) {
         Ok(content) => print!("{}\n", content),
-        Err(reason) => print!("error: {}\n", reason),
-    }
+        Err (err) => print!("error: {}\n", err)
+    };
+}
+
+fn main() {
+    let path = "./src/main.rs".to_string();
+    run_cat(path);
 }
