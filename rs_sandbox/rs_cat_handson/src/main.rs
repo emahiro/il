7807aabc67e9a17e62dbe417ee70e671ddd33f98
emahiro <ema.hiro@gmail.com>
2021-07-27 23:19:00 +0900

    rm: patten match sample

--- rs_sandbox/rs_cat_handson/src/main.rs
+++ rs_sandbox/rs_cat_handson/src/main.rs
@@ -8,11 +8,6 @@ fn run_cat(path: String) {
 }
 
 fn main() {
-    let x: Option<i32> = Some(10);
-    match x {
-        Some(n) => print!("found {}\n", n),
-        None => print!("none")
-    };
     match args().nth(1) {
         Some(path) => run_cat(path),
         None => print!("No path is specified\n")
