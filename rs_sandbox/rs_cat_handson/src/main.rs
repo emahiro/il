0643b39f8103efb0afa14b041348bedf4f95612c
emahiro <ema.hiro@gmail.com>
2021-07-27 20:46:07 +0900

    fix: add EOL

--- rs_sandbox/rs_cat_handson/src/main.rs
+++ rs_sandbox/rs_cat_handson/src/main.rs
@@ -3,7 +3,7 @@ use std::fs::read_to_string;
 fn main() {
     let path = "./src/main.rs".to_string();
     match read_to_string(path) {
-        Ok(content) => print!("{}", content),
-        Err(reason) => print!("error: {}", reason),
+        Ok(content) => print!("{}\n", content),
+        Err(reason) => print!("error: {}\n", reason),
     }
 }
