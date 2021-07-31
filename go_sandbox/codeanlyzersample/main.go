1eada5667a829f5f2761faec21d7140ea4fefdab
emahiro <ema.hiro@gmail.com>
2021-08-01 03:15:28 +0900

    add: code checker template

--- go_sandbox/codeanlyzersample/main.go
+++ go_sandbox/codeanlyzersample/main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/analysis/singlechecker"
+	"golang.org/x/tools/go/ast/inspector"
+)
+
+var a = &analysis.Analyzer{
+	Name:     "samplechecher",
+	Doc:      "sample code checker",
+	Run:      run,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
+}
+
 func main() {
+	singlechecker.Main(a)
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	i := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	filter := []ast.Node{}
+	i.Preorder(filter, func(n ast.Node) {})
+	return nil, nil
 }
