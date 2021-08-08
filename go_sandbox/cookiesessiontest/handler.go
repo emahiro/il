f563a1824e2cd7540b1df2f0e076308336a98d07
emahiro <ema.hiro@gmail.com>
2021-08-08 22:00:57 +0900

    add: obtain cookie

--- go_sandbox/cookiesessiontest/handler.go
+++ go_sandbox/cookiesessiontest/handler.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Handler struct{}
+
+func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
+	ck, err := r.Cookie("test")
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(http.StatusText(http.StatusUnauthorized)))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("OK. cookie value is %#v", ck)))
+}
