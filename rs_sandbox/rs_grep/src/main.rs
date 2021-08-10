529dcbfda5cb0968d7da6212b96441c59c058cab
emahiro <ema.hiro@gmail.com>
2021-08-10 18:00:54 +0900

    fix: refactor

--- rs_sandbox/rs_grep/src/main.rs
+++ rs_sandbox/rs_grep/src/main.rs
@@ -7,23 +7,23 @@ struct GrepArgs {
     #[structopt(name = "PATTERN")]
     pattern: String,
     #[structopt(name = "FILEPATH")]
-    filepath: Vec<String>,
+    files: Vec<String>,
 }
 
 impl GrepArgs {}
 
-fn grep(content: String, state: &GrepArgs, file_name:&String) {
+fn grep(content: String, state: &GrepArgs, file_name: &String) {
     for line in content.lines() {
         if line.contains(state.pattern.as_str()) {
-            println!("{}: {}\n",file_name, line);
+            println!("{}: {}\n", file_name, line);
         }
     }
 }
 
 fn run(state: GrepArgs) {
-    for filepath in state.filepath.iter() {
-        match read_to_string(filepath) {
-            Ok(content) => grep(content, &state, filepath),
+    for file in state.files.iter() {
+        match read_to_string(file) {
+            Ok(content) => grep(content, &state, file),
             Err(err) => println!("error: {}\n", err),
         }
     }
