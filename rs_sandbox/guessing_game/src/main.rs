dada60ab0fec197b3e90cad0d07e4e4719022030
emahiro <ema.hiro@gmail.com>
2021-08-13 00:46:45 +0900

    add: generate secret number

--- rs_sandbox/guessing_game/src/main.rs
+++ rs_sandbox/guessing_game/src/main.rs
@@ -1,7 +1,14 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
     println!("Guess the number.");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -10,5 +17,5 @@ fn main() {
         .read_line(&mut guess)
         .expect("failed to read line.");
 
-    println!("You guessed: {}", guess)
+    println!("You guessed: {}", guess);
 }
