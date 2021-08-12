2563b418e958c25d911a84b3eea3064c9cd23d08
emahiro <ema.hiro@gmail.com>
2021-08-13 00:58:05 +0900

    add: loop & be able to guess number until match to secret number

--- rs_sandbox/guessing_game/src/main.rs
+++ rs_sandbox/guessing_game/src/main.rs
@@ -9,21 +9,23 @@ fn main() {
 
     println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line.");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line.");
 
-    let guess: u32 = guess.trim().parse().expect("failed to parse guess number");
+        let guess: u32 = guess.trim().parse().expect("failed to parse guess number");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        std::cmp::Ordering::Less => println!("Too small"),
-        std::cmp::Ordering::Equal => println!("You win !!!"),
-        std::cmp::Ordering::Greater => println!("Too Big"),
-    };
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small"),
+            std::cmp::Ordering::Equal => println!("You win !!!"),
+            std::cmp::Ordering::Greater => println!("Too Big"),
+        };
+    }
 }
