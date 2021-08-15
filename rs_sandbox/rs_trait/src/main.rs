9410b24725f8d63ea6f2f7bbf5f334ae61bf6625
emahiro <ema.hiro@gmail.com>
2021-08-15 20:26:44 +0900

    add: trait

--- rs_sandbox/rs_trait/src/main.rs
+++ rs_sandbox/rs_trait/src/main.rs
@@ -1,3 +1,39 @@
+trait Animal {
+    fn bark(&self);
+    fn life_span(&self) -> String;
+}
+
+struct Dog {}
+
+impl Animal for Dog {
+    fn bark(&self) {
+        println!("BowWow");
+    }
+
+    fn life_span(&self) -> String {
+        return "13".to_string();
+    }
+}
+
+struct Cat {}
+
+impl Animal for Cat {
+    fn bark(&self) {
+        println!("NyauNayu");
+    }
+
+    fn life_span(&self) -> String {
+        return "30".to_string();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let dog = Dog {};
+    dog.bark();
+
+    let cat = Cat {};
+    cat.bark();
+
+    println!("{}", dog.life_span());
+    println!("{}", cat.life_span())
 }
