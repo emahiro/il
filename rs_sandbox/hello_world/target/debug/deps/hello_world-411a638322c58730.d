50f7319f9b40c9d9de1b744dee7ac52995aea79a
emahiro <ema.hiro@gmail.com>
2021-08-15 22:38:55 +0900

    fix: rm dist

--- rs_sandbox/hello_world/target/debug/deps/hello_world-411a638322c58730.d
+++
@@ -1,5 +0,0 @@
-/Users/hiromichi.ema/src/github.com/emahiro/il/rs_sandbox/hello_world/target/debug/deps/hello_world-411a638322c58730.rmeta: src/main.rs
-
-/Users/hiromichi.ema/src/github.com/emahiro/il/rs_sandbox/hello_world/target/debug/deps/hello_world-411a638322c58730.d: src/main.rs
-
-src/main.rs:
