859137237d13bfd2f318f6cf6d1161463b3ff490
emahiro <ema.hiro@gmail.com>
2021-08-22 22:00:42 +0900

    add: create some threads
    - このタイミングでは x の寿命より thread (handle) が長くなるかもしれず、x が解放された後に handle から参照してしまうケースがあるので、lambda 式の中に x の所有権を移す必要がある。

--- rs_sandbox/rs_multi_thread_study/src/main.rs
+++ rs_sandbox/rs_multi_thread_study/src/main.rs
@@ -4,11 +4,20 @@ use std::{
 };
 
 fn main() {
-    let handle = thread::spawn(|| {
-        println!("hello world");
-    });
-    match dbg!(handle.join()) {
-        Err(e) => println!("err: {:?}", e),
-        _ => (),
+    // create 10 threads
+    let mut handles = Vec::new();
+
+    for x in 0..10{
+        let handle = thread::spawn(|| {
+            println!("hello world! - {}", x);
+        });
+        handles.push(handle)
+    }
+
+    for handle in handles {
+        match dbg!(handle.join()) {
+            Err(e) => println!("err: {:?}", e),
+            _ => (),
+        }
     }
 }
