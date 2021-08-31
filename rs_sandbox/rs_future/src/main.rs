1560c53a84b624590a7b4e2d824f80fe4602f876
emahiro <ema.hiro@gmail.com>
2021-09-01 02:44:40 +0900

    add: raw future
    - executor::block_on は渡された Future が完了するまで処理を待つ（blockする）API
    - この場合は定義した二つの Future が回り切るまで処理を続けてくれる。
    - future は vec![] で定義することでいくらでも増やすことが可能。

--- rs_sandbox/rs_future/src/main.rs
+++ rs_sandbox/rs_future/src/main.rs
@@ -1,3 +1,31 @@
+use std::{pin::Pin, task::{Context, Poll}};
+
+use futures::{Future, executor, future::join_all};
+
+struct CountDown(u32);
+
+impl Future for CountDown {
+    type Output = String;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<String>{
+        if self.0 == 0{
+            Poll::Ready("ZERO!!!".to_string())
+        }else {
+            println!("{}", self.0);
+            self.0 -= 1;
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let countdown_future1 = CountDown(10);
+    let countdown_future2 = CountDown(20);
+    let cd_set = join_all(vec![countdown_future1, countdown_future2]);
+    let res = executor::block_on(cd_set);
+    for (i, s) in res.iter().enumerate(){
+        println!("{}: {}", i, s);
+    }
+
 }
