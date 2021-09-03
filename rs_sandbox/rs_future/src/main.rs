a4d5138c9dcfe928a1c9e3c76911ee5c5a2dfdd4
emahiro <ema.hiro@gmail.com>
2021-09-04 03:44:47 +0900

    learned async/await
    await メソッドは async 関数内でしか使えない。async で定義した関数を使うときは executor::block_on を使う

--- rs_sandbox/rs_future/src/main.rs
+++ rs_sandbox/rs_future/src/main.rs
@@ -1,17 +1,20 @@
-use std::{pin::Pin, task::{Context, Poll}};
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
 
-use futures::{Future, executor, future::join_all};
+use futures::{executor, future::join_all, Future};
 
 struct CountDown(u32);
 
 impl Future for CountDown {
     type Output = String;
 
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<String>{
-        if self.0 == 0{
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<String> {
+        if self.0 == 0 {
             Poll::Ready("ZERO!!!".to_string())
-        }else {
-            println!("{}", self.0);
+        } else {
+            // println!("{}", self.0);
             self.0 -= 1;
             cx.waker().wake_by_ref();
             Poll::Pending
@@ -19,13 +22,25 @@ impl Future for CountDown {
     }
 }
 
+async fn async_add(left: i32, right: i32) -> i32 {
+    return left + right;
+}
+
+async fn something_great_async_function() -> i32 {
+    let ans = async_add(1,1).await;
+    println!("answer = {}\n", ans);
+    return ans
+}
+
 fn main() {
-    let countdown_future1 = CountDown(10);
-    let countdown_future2 = CountDown(20);
-    let cd_set = join_all(vec![countdown_future1, countdown_future2]);
-    let res = executor::block_on(cd_set);
-    for (i, s) in res.iter().enumerate(){
-        println!("{}: {}", i, s);
-    }
+    // let countdown_future1 = CountDown(10);
+    // let countdown_future2 = CountDown(20);
+    // let cd_set = join_all(vec![countdown_future1, countdown_future2]);
+    // let _ = executor::block_on(cd_set);
+    // for (i, s) in res.iter().enumerate(){
+    //     println!("{}: {}", i, s);
+    // }
+
+    let _ = executor::block_on(something_great_async_function());
 
 }
