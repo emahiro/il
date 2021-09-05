8e14acac53e9dd3d220d7404ed0d0f97b7412f30
emahiro <ema.hiro@gmail.com>
2021-09-05 23:35:15 +0900

    add: connection with mini_redis

--- rs_sandbox/rs_tokio/src/main.rs
+++ rs_sandbox/rs_tokio/src/main.rs
@@ -1,7 +1,12 @@
-use mini_redis::{Result};
+use mini_redis::{Result, client};
 
 #[tokio::main]
 pub async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+    client.set("hello", "world".into()).await?;
+    let result = client.get("hello").await?;
+
+    println!("get value from the server; result = {:?}", result);
 
     Ok(())
 }
