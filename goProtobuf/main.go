48626af9eb06507420085d7bed185a2bc42d95ce
emahiro <ema.hiro@gmail.com>
2023-06-02 05:09:09 +0900

    fix: json string

--- goProtobuf/main.go
+++ goProtobuf/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/emahiro/il/protobuf/pb/tutrialpb"
+)
 
 func main() {
-	fmt.Println("hello world")
+
+	person := tutrialpb.Person{
+		Name:  "Taro",
+		Id:    1,
+		Email: "taro@examle.com",
+		Phones: []*tutrialpb.Person_PhoneNumber{
+			{
+				Number: "090-1111-1111",
+				Type:   tutrialpb.Person_MOBILE,
+			},
+		},
+	}
+	b, err := json.Marshal(&person)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(b))
 }
