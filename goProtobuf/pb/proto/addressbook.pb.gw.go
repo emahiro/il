daf64895caf0c102f0ba25dd177428d2ee01510d
emahiro <ema.hiro@gmail.com>
2023-06-02 19:47:02 +0900

    add: get endpoint

--- goProtobuf/pb/proto/addressbook.pb.gw.go
+++ goProtobuf/pb/proto/addressbook.pb.gw.go
@@ -31,6 +31,76 @@ var _ = runtime.String
 var _ = utilities.NewDoubleArray
 var _ = metadata.Join
 
+var (
+	filter_AddressBookService_GetPerson_0 = &utilities.DoubleArray{Encoding: map[string]int{"name": 0}, Base: []int{1, 2, 0, 0}, Check: []int{0, 1, 2, 2}}
+)
+
+func request_AddressBookService_GetPerson_0(ctx context.Context, marshaler runtime.Marshaler, client AddressBookServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq Person
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["name"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "name")
+	}
+
+	protoReq.Name, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "name", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AddressBookService_GetPerson_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetPerson(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_AddressBookService_GetPerson_0(ctx context.Context, marshaler runtime.Marshaler, server AddressBookServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq Person
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["name"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "name")
+	}
+
+	protoReq.Name, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "name", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AddressBookService_GetPerson_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetPerson(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_AddressBookService_AddPerson_0(ctx context.Context, marshaler runtime.Marshaler, client AddressBookServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq Person
 	var metadata runtime.ServerMetadata
@@ -70,6 +140,31 @@ func local_request_AddressBookService_AddPerson_0(ctx context.Context, marshaler
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterAddressBookServiceHandlerFromEndpoint instead.
 func RegisterAddressBookServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server AddressBookServiceServer) error {
+
+	mux.Handle("GET", pattern_AddressBookService_GetPerson_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/tutrial.AddressBookService/GetPerson", runtime.WithHTTPPathPattern("/v1/people/{name}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_AddressBookService_GetPerson_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_AddressBookService_GetPerson_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
 
 	mux.Handle("POST", pattern_AddressBookService_AddPerson_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
@@ -137,6 +232,28 @@ func RegisterAddressBookServiceHandler(ctx context.Context, mux *runtime.ServeMu
 // "AddressBookServiceClient" to call the correct interceptors.
 func RegisterAddressBookServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client AddressBookServiceClient) error {
 
+	mux.Handle("GET", pattern_AddressBookService_GetPerson_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/tutrial.AddressBookService/GetPerson", runtime.WithHTTPPathPattern("/v1/people/{name}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_AddressBookService_GetPerson_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_AddressBookService_GetPerson_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_AddressBookService_AddPerson_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -163,9 +280,13 @@ func RegisterAddressBookServiceHandlerClient(ctx context.Context, mux *runtime.S
 }
 
 var (
+	pattern_AddressBookService_GetPerson_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "people", "name"}, ""))
+
 	pattern_AddressBookService_AddPerson_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "people"}, ""))
 )
 
 var (
+	forward_AddressBookService_GetPerson_0 = runtime.ForwardResponseMessage
+
 	forward_AddressBookService_AddPerson_0 = runtime.ForwardResponseMessage
 )
--- goProtobuf/pb/proto/addressbook_grpc.pb.go
+++ goProtobuf/pb/proto/addressbook_grpc.pb.go
@@ -19,6 +19,7 @@ import (
 const _ = grpc.SupportPackageIsVersion7
 
 const (
+	AddressBookService_GetPerson_FullMethodName = "/tutrial.AddressBookService/GetPerson"
 	AddressBookService_AddPerson_FullMethodName = "/tutrial.AddressBookService/AddPerson"
 )
 
@@ -26,6 +27,7 @@ const (
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type AddressBookServiceClient interface {
+	GetPerson(ctx context.Context, in *Person, opts ...grpc.CallOption) (*Person, error)
 	AddPerson(ctx context.Context, in *Person, opts ...grpc.CallOption) (*Person, error)
 }
 
@@ -35,6 +37,15 @@ type addressBookServiceClient struct {
 
 func NewAddressBookServiceClient(cc grpc.ClientConnInterface) AddressBookServiceClient {
 	return &addressBookServiceClient{cc}
+}
+
+func (c *addressBookServiceClient) GetPerson(ctx context.Context, in *Person, opts ...grpc.CallOption) (*Person, error) {
+	out := new(Person)
+	err := c.cc.Invoke(ctx, AddressBookService_GetPerson_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
 func (c *addressBookServiceClient) AddPerson(ctx context.Context, in *Person, opts ...grpc.CallOption) (*Person, error) {
@@ -50,6 +61,7 @@ func (c *addressBookServiceClient) AddPerson(ctx context.Context, in *Person, op
 // All implementations must embed UnimplementedAddressBookServiceServer
 // for forward compatibility
 type AddressBookServiceServer interface {
+	GetPerson(context.Context, *Person) (*Person, error)
 	AddPerson(context.Context, *Person) (*Person, error)
 	mustEmbedUnimplementedAddressBookServiceServer()
 }
@@ -58,6 +70,9 @@ type AddressBookServiceServer interface {
 type UnimplementedAddressBookServiceServer struct {
 }
 
+func (UnimplementedAddressBookServiceServer) GetPerson(context.Context, *Person) (*Person, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetPerson not implemented")
+}
 func (UnimplementedAddressBookServiceServer) AddPerson(context.Context, *Person) (*Person, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method AddPerson not implemented")
 }
@@ -72,6 +87,24 @@ type UnsafeAddressBookServiceServer interface {
 
 func RegisterAddressBookServiceServer(s grpc.ServiceRegistrar, srv AddressBookServiceServer) {
 	s.RegisterService(&AddressBookService_ServiceDesc, srv)
+}
+
+func _AddressBookService_GetPerson_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Person)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AddressBookServiceServer).GetPerson(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: AddressBookService_GetPerson_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AddressBookServiceServer).GetPerson(ctx, req.(*Person))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 func _AddressBookService_AddPerson_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -99,6 +132,10 @@ var AddressBookService_ServiceDesc = grpc.ServiceDesc{
 	ServiceName: "tutrial.AddressBookService",
 	HandlerType: (*AddressBookServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "GetPerson",
+			Handler:    _AddressBookService_GetPerson_Handler,
+		},
 		{
 			MethodName: "AddPerson",
 			Handler:    _AddressBookService_AddPerson_Handler,
