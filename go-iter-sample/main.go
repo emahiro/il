9317ad97efc0736510153a9aacf1b1eb2419fb14
emahiro <ema.hiro@gmail.com>
2024-08-14 20:38:39 +0900

    add: sortFunc の挙動の研究

--- go-iter-sample/main.go
+++ go-iter-sample/main.go
@@ -2,17 +2,49 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/emahiro/il/go-iter-sample/list"
+	"maps"
+	"slices"
 )
 
-func main() {
-	var l list.List[int]
-	l.Push(1)
-	l.Push(2)
-	l.Push(3)
+type taple struct {
+	a int
+	b string
+}
 
-	for list := range l.All() {
-		fmt.Println(list.Value())
+func main() {
+	num := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	for i := range slices.Chunk(num, 3) {
+		fmt.Println(i)
 	}
+
+	m := map[string]int{"a": 1, "b": 2, "c": 3}
+	for k, v := range maps.All(m) {
+		fmt.Println(k, v)
+	}
+
+	// random slice order
+	arr := []int{3, 1, 2, 4, 5}
+	fmt.Println(slices.IsSorted(arr))
+	slices.SortFunc(arr, func(a, b int) int {
+		if a < b {
+			return -1
+		} else if a > b {
+			return 1
+		}
+		return 0
+	})
+	fmt.Println(arr)
+
+	// sort の種類を設定してみる
+	taples1 := []taple{{3, "dd"}, {1, "a"}, {3, "cc"}, {3, "ff"}, {2, "b"}, {4, "ee"}}
+	s1 := slices.SortedFunc(slices.Values(taples1), func(a, b taple) int {
+		return a.a - b.a
+	})
+	fmt.Println(s1)
+
+	taples2 := []taple{{3, "dd"}, {1, "a"}, {3, "cc"}, {3, "ff"}, {2, "b"}, {4, "ee"}}
+	s2 := slices.SortedStableFunc(slices.Values(taples2), func(a, b taple) int {
+		return a.a - b.a
+	})
+	fmt.Println(s2)
 }
