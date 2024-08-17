1f8af108ec290ff087cdd429841d319702adf681
emahiro <ema.hiro@gmail.com>
2024-08-17 16:19:17 +0900

    add: info logger

--- sloger/sloger/sloger.go
+++ sloger/sloger/sloger.go
@@ -1,6 +1,8 @@
 package sloger
 
 import (
+	"context"
+	"fmt"
 	"log/slog"
 	"os"
 )
@@ -9,4 +11,9 @@ var logger *slog.Logger
 
 func New() {
 	logger = slog.New(slog.NewJSONHandler(os.Stderr, nil))
+}
+
+func Infof(ctx context.Context, format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	logger.LogAttrs(ctx, slog.LevelInfo, msg)
 }
